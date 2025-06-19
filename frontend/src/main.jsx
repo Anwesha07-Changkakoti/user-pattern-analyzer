ef45d24a68cee737d67751720779a2e443789fde
Anwesha Changkakoti <anweshachangkakoti2003@gmail.com>
2025-06-20 01:44:49 +0530

    Reset frontend with working Vite setup

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./components/App";
 import { AuthProvider } from "./contexts/AuthContext";
-import "./index.css";
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
